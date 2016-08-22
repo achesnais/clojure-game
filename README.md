c84198246ee9a17e4745417116a00dcb4368d722
Antoine Chesnais <achesnais@gmail.com>
2016-08-22 19:21:40 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# clojure-game
+
+## License
+
+Copyright © 2016 Antoine Chesnais
+
+Distributed under the Eclipse Public License version 1.0.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clojure-game "0.1.0-SNAPSHOT"
+  :description "A Sandbox for a game in Clojure."
+  :url "https://github.com/achesnais/clojure-game"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
